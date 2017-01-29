3c268c3083aab61c8613e18a623834f74d2d2ca9
Loris Guerra <loris4526@gmail.com>
2017-01-29 23:33:22 +0100

    Add filter functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -56,7 +56,8 @@ var TodoApp = React.createClass({
 
   render: function(){
 
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos,showCompleted,searchText);
 
     return(
       <div>
@@ -64,7 +65,7 @@ var TodoApp = React.createClass({
           <div className="column small-centered medium-6 large-4">
             <h3>TodoApp.jsx</h3>
             <TodoSearch onSearch={this.handleSearch}/>
-            <TodoList todos={todos} onToggle={this.handleToggle}/>
+            <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
             <br/>
             <AddTodo onAddTodo={this.handleAddTodo}/>
           </div>
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -67,4 +67,55 @@ describe('TodoAPI', ()=>{
 
   });
 
+  describe('filterTodos', ()=>{
+    var todos = [
+      {
+      id:1,
+      text:'some text',
+      completed:true
+    },{
+      id:2,
+      text:'other text',
+      completed:false
+    },{
+      id:3,
+      text:'some text',
+      completed:true
+    }
+  ];
+
+    it('should return all items if showCompleted is true',()=>{
+      var filterTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filterTodos.length).toBe(3);
+    });
+
+    it('should return only an item if showCompleted is false',()=>{
+      var filterTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filterTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status',()=>{
+      var filterTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filterTodos[0].completed).toBe(false);
+    });
+
+    it('should return all items if searchItem is empty',()=>{
+      var filterTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filterTodos.length).toBe(3);
+    });
+
+    it('should return the 2 items when searching "some"',()=>{
+      var filterTodos = TodoAPI.filterTodos(todos, true, 'some');
+
+      expect(filterTodos.length).toBe(2);
+    });
+
+
+
+    // it('',()=>{});
+
+  });
+
 });
