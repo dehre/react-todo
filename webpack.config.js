88b28f73f2d9e4d36ad37cf9a758277101bfaf40
Loris Guerra <loris4526@gmail.com>
2017-01-31 19:52:29 +0100

    Add todos Reducer and test

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ var config = {
     ],
     alias: {
       applicationStyles: 'app/styles/app.scss',
-      actions: 'app/actions/actions.jsx'
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx'
     },
     extensions: ['','.js','.jsx']
   },
