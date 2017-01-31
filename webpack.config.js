d3786643242c3f2c13772a003c2b64eca23f4170
Loris Guerra <loris4526@gmail.com>
2017-01-31 20:27:30 +0100

    set up redux store

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ var config = {
     alias: {
       applicationStyles: 'app/styles/app.scss',
       actions: 'app/actions/actions.jsx',
-      reducers: 'app/reducers/reducers.jsx'
+      reducers: 'app/reducers/reducers.jsx',
+      configureStore: 'app/store/configureStore.jsx'
     },
     extensions: ['','.js','.jsx']
   },
