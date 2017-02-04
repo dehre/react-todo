9dd25e55c51f8cda8a72ea90da29b347bbc0de84
Loris Guerra <loris4526@gmail.com>
2017-02-04 12:07:09 +0100

    Add auth reducer and actions

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -86,4 +86,31 @@ describe('Reducers',()=>{
 
   });
 
+  describe('authReducer',()=>{
+    it('should store uid on LOGIN',()=>{
+      const action = {
+        type: 'LOGIN',
+        uid: 'abc'
+      };
+      const res = reducers.authReducer(undefined, df(action));
+
+      expect(res).toEqual({
+        uid: action.uid
+      });
+    });
+
+    it('should wipe off on LOGOUT',()=>{
+      const authData = {
+        uid: 'abc'
+      };
+      const action = {
+        type: 'LOGOUT'
+      };
+      const res = reducers.authReducer(df(authData),df(action));
+
+      expect(res).toEqual({});
+    });
+
+  });
+
 });
